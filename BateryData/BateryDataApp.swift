a19638e084c48ea3110a8524b254d32d18086d12
iziuk <dmytro@echoedai.com>
2025-09-11 17:41:05 +0200

    Initial Commit

---
+++ BateryData/BateryDataApp.swift
@@ -0,0 +1,17 @@
+//
+//  BateryDataApp.swift
+//  BateryData
+//
+//  Created by Dmytro Izyuk on 11.09.2025.
+//
+
+import SwiftUI
+
+@main
+struct BateryDataApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ BateryDataTests/BateryDataTests.swift
@@ -0,0 +1,17 @@
+//
+//  BateryDataTests.swift
+//  BateryDataTests
+//
+//  Created by Dmytro Izyuk on 11.09.2025.
+//
+
+import Testing
+@testable import BateryData
+
+struct BateryDataTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
